a8f18a3f04d607906b311c43b7f28933e7c26d6d
Jarauta1 <diegojarauta8@gmail.com>
2021-11-02 16:49:38 +0100

    first model (server)

--- l4gapi/models.py
+++ l4gapi/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+# Server table with fields id (primary key), server name, members and groups
+
+class Server(models.Model):
+
+    # Properties
+    # Automatic ID, increases by one value with each new record
+    server_id = models.BigAutoField(primary_key=True)
+    server_name = models.CharField(max_length=50)
+    members = models.JSONField(blank=True, default=dict)
+    groups = models.JSONField(blank=True, default=dict)
+
+    # Magic method
+    def __str__(self):
+        return self.server_name
